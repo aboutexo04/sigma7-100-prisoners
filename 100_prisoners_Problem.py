9f794919e9ef7d7ba9b940c73192a94422cc001f
aboutexo04 <aboutexo04@gmail.com>
2025-08-22 18:07:58 +0900

    feat:Complete code

--- 100_prisoners_Problem.py
+++ 100_prisoners_Problem.py
@@ -1,1 +1,5 @@
-print("start")
+if __name__ == '__main__':
+    n = 100_000
+    print(" Simulation count:", n)
+    print(f" Random play wins: {play_random(n):4.1f}% of simulations")
+    print(f"Optimal play wins: {play_optimal(n):4.1f}% of simulations")
